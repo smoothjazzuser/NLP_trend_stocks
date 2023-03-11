62e3298d31096793e7a2244379905adaec7bd149
smoothjazzuser <9345653+smoothjazzuser@users.noreply.github.com>
2023-03-11 08:14:39 -0600

    added regulizers to model

--- models.py
+++ models.py
@@ -22,8 +22,10 @@ def siamese_model(hp):
     """
     
     activity_regularizer = hp.Choice('activity_regularizer', ['None', 'l1', 'l2'])
-    weight_regularizer = hp.Choice('weight_regularizer', ['None', 'l1', 'l2'])
+    bias_regularizer = hp.Choice('bias_regularizer', ['None', 'l1', 'l2'])
     kernel_regularizer = hp.Choice('kernel_regularizer', ['None', 'l1', 'l2'])
+    kernel_constraint = hp.Choice('kernel_constraint', ['None', 'max_norm', 'min_max_norm', 'non_neg', 'unit_norm', 'unitwise_norm'])
+    bias_constraint = hp.Choice('bias_constraint', ['None', 'max_norm', 'min_max_norm', 'non_neg', 'unit_norm', 'unitwise_norm'])
     activation = hp.Choice('activation',             ['gelu', 'selu', 'tanh', 'softplus', 'swish'])
     activation_first = hp.Choice('activation_first', ['gelu', 'selu', 'tanh', 'softplus', 'swish']) 
     activation_head = hp.Choice('activation_last',   ['gelu', 'selu', 'tanh', 'softplus', 'swish'])  
@@ -40,16 +42,25 @@ def siamese_model(hp):
     neuron_sizes_1st = np.linspace(neurons_start, neurons_middle, num_layers, dtype=int) # interpolate first half of layers sizes
     neuron_sizes_2nd = np.linspace(neurons_middle, neurons_end, num_layers, dtype=int) # interpolate second half of layers sizes
     neuron_sizes = list(set(neuron_sizes_1st).union(set(neuron_sizes_2nd))) # take the union of the two sets of layer sizes
-    activations = [activation_first] + [activation] * (num_layers - 2) + [activation_head] 
+    
     if activity_regularizer != 'None':
         activity_regularizer = getattr(keras.regularizers, activity_regularizer)
-    if weight_regularizer != 'None':
-        weight_regularizer = getattr(keras.regularizers, weight_regularizer)
+    if bias_regularizer != 'None':
+        bias_regularizer = getattr(keras.regularizers, bias_regularizer)
     if kernel_regularizer != 'None':
         kernel_regularizer = getattr(keras.regularizers, kernel_regularizer)
+    if kernel_constraint != 'None':
+        kernel_constraint = getattr(keras.constraints, kernel_constraint)
+    if bias_constraint != 'None':
+        bias_constraint = getattr(keras.constraints, bias_constraint)
+
     activity_regularizer = [activity_regularizer] * (num_layers - 1) + [None]
-    weight_regularizer = [weight_regularizer] * (num_layers - 1) + [None]
+    bias_regularizer = [bias_regularizer] * (num_layers - 1) + [None]
     kernel_regularizer = [kernel_regularizer] * (num_layers - 1) + [None]
+    kernel_constraint = [kernel_constraint] * (num_layers - 1) + [None]
+    bias_constraint = [bias_constraint] * (num_layers - 1) + [None]
+    activations = [activation_first] + [activation] * (num_layers - 2) + [activation_head] 
+
 
 
     inp1 = layers.Input(shape=x_shape)
@@ -63,8 +74,11 @@ def siamese_model(hp):
             neuron_sizes[i], 
             activation=activations[i],
             activity_regularizer=activity_regularizer[i],
-            weight_regularizer=weight_regularizer[i],
-            kernel_regularizer=kernel_regularizer[i]
+            bias_regularizer=bias_regularizer[i],
+            kernel_regularizer=kernel_regularizer[i],
+            kernel_constraint=kernel_constraint[i],
+            bias_constraint=bias_constraint[i],
+            use_bias=True,
         ))
     
     vec1 = shared_weights(inp1)
