ce63c24f158c11a1efdecbe67062af7e764f13d0
smoothjazzuser <9345653+smoothjazzuser@users.noreply.github.com>
2023-03-12 08:33:21 -0500

    update tuning perameters

--- models.py
+++ models.py
@@ -15,29 +15,19 @@ def triplet_loss(dist1, dist2, dist3):
     
 
 def siamese_model(hp):
-    x_shape, label_shape = ((300,), (29,))
-
     """
     Set of hyperperameters and neural architechure to tune
     """
-    
-    activity_regularizer = hp.Choice('activity_regularizer', [True, False])
-    bias_regularizer = hp.Choice('bias_regularizer', [True, False])
-    kernel_regularizer = hp.Choice('kernel_regularizer', [True, False])
-    kernel_constraint = hp.Choice('kernel_constraint', [True, False])
-    bias_constraint = hp.Choice('bias_constraint', [True, False])
-    reg_level = hp.Choice('reg_level', [0.01, 0.001, 0.0001])
-    constraint_max = hp.Choice('constraint_max', [0.1, 0.5, 1.0, 2.0, 3.0])
-    constraint_min = hp.Choice('constraint_min', [0.0, 0.05, 0.1])
-    constraint_rate = hp.Choice('constraint_rate', [0.05, 0.1, 0.5, 0.8, 1.0])
-    activation = hp.Choice('activation',             ['gelu', 'selu', 'tanh', 'softplus', 'swish'])
-    activation_first = hp.Choice('activation_first', ['gelu', 'selu', 'tanh', 'softplus', 'swish']) 
-    activation_head = hp.Choice('activation_last',   ['gelu', 'selu', 'tanh', 'softplus', 'swish'])  
+    x_shape, label_shape = ((300,), (29,))
+
+    activation = 'gelu'
+    activation_first = 'selu'
+    activation_head = 'gelu'
     learning_rate = 0.001
     num_layers = 7
-    neurons_start = hp.Int('neurons_start', 64, 120, step=4) # steps based in the range of the best hyperparameters from a previous courser run
-    neurons_middle = hp.Int('neurons_middle', 120, 240, step=4)
-    neurons_end = hp.Int('neurons_end', 120, 300, step=4)
+    neurons_start = hp.Int('neurons_start', 72 - 24, 72 + 24, step=2) #72
+    neurons_middle = hp.Int('neurons_middle', 168 - 42, 168 + 42, step=2) # 168
+    neurons_end = hp.Int('neurons_end', 224 - 42, 224 + 42, step=2) # 224
 
     """
     Generally in models, the number of neurons in each layer is increasing or decreasing (with a possible change around the midpoint of the model)
@@ -48,17 +38,6 @@ def siamese_model(hp):
     neuron_sizes = list(set(neuron_sizes_1st).union(set(neuron_sizes_2nd))) # take the union of the two sets of layer sizes
 
     activations = [activation_first] + [activation] * (num_layers - 2)
-
-    if activity_regularizer: activity_regularizer = tf.keras.regularizers.L2(reg_level)
-    else: activity_regularizer = None
-    if bias_regularizer: bias_regularizer = tf.keras.regularizers.L2(reg_level)
-    else: bias_regularizer = None
-    if kernel_regularizer: kernel_regularizer = tf.keras.regularizers.L2(reg_level)
-    else: kernel_regularizer = None
-    if kernel_constraint: kernel_constraint = tf.keras.constraints.MinMaxNorm(constraint_max, constraint_min)
-    else: kernel_constraint = None
-    if bias_constraint: bias_constraint = tf.keras.constraints.MinMaxNorm(constraint_max, constraint_min)
-    else: bias_constraint = None
 
 
     inp1 = layers.Input(shape=x_shape)
@@ -74,11 +53,6 @@ def siamese_model(hp):
             shared_weights.add(layers.Dense(
                 neuron_sizes[i], 
                 activation=activations[i],
-                activity_regularizer=activity_regularizer,
-                bias_regularizer=bias_regularizer,
-                kernel_regularizer=kernel_regularizer,
-                kernel_constraint=kernel_constraint,
-                bias_constraint=bias_constraint,
                 use_bias=True,
             ))
     
